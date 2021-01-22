622f5d590088d46795023f05a3335dae1259ec6c
rhammock1 <rhamm1027@gmail.com>
2021-01-22 12:07:33 -0500

    added pet services- created queues

--- src/helpers/queue.js
+++ src/helpers/queue.js
@@ -1,4 +1,4 @@
-'use-strict';
+'use strict';
 
 class _Node {
   constructor(value) {
---
+++ src/pets/pets-service.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Queue = require('../helpers/queue');
+const { cats, dogs } = require('../helpers/store');
+
+const pets = {
+  cats: new Queue(),
+  dogs: new Queue(),
+};
+
+cats.forEach((cat) => pets.cats.enqueue(cat));
+dogs.forEach((dog) => pets.dogs.enqueue(dog));
+
+const getTopValue = function() {
+  return {
+    cat: pets.cats.displayTop(),
+    dog: pets.dogs.displayTop()
+  }
+};
+
+const dequeue = function(type) {
+  return pets[`${type}`].dequeue();
+};
+
+module.exports = { getTopValue, dequeue };
