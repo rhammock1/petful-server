9726408b93c78a5a4e83e40fb75b8edd851bf763
rhammock1 <rhamm1027@gmail.com>
2021-01-22 11:58:16 -0500

    Added queue class and moved store to helper folder

---
+++ src/helpers/queue.js
@@ -0,0 +1,71 @@
+'use-strict';
+
+class _Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  enqueue(value) {
+    const node = new _Node(value);
+
+    this.length++;
+    if (this.first === null) {
+      this.first = node;
+    }
+    if (this.last) {
+      this.last.next = node;
+    }
+    this.last = node;
+    return node.value;
+  }
+
+  dequeue() {
+    if (this.first === null) {
+      return;
+    }
+    this.length--;
+    
+    const node = this.first;
+    this.first = this.first.next;
+
+    if (node === this.last) {
+      this.last = null;
+    }
+
+    return node.value;
+  }
+
+  displayTop() {
+    if (this.first === null) {
+      return null;
+    }
+    return this.first.value;
+  }
+
+  displayAll() {
+    const values = [];
+    let done = false;
+    let currentNode = this.first;
+
+    while (done === false) {
+      if(currentNode === null) {
+        done = true;
+        break;
+      }
+      values.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return values;
+  }
+}
+
+module.exports = Queue;
